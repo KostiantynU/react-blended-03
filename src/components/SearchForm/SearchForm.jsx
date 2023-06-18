e82de05efb2b9160fea2b4fe623e403ad4c1cd10
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-06-18 16:03:08 +0300

    Add search countries

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,31 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
-export const SearchForm = () => {
+export const SearchForm = ({onSubmit}) => {
+  const [region, setRegion] = useState('');
+
+  const onChange = (e) => {
+    setRegion(e.target.value);
+  }
+
+const handleSubmit =(e) => {
+  e.preventDefault();
+  if(!region) {
+    alert('Select any region');
+    return;
+  }
+  onSubmit(region);
+
+}
+
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select defaultValue='default' aria-label="select" name="region" required onChange={onChange}>
+        <option value='default' disabled defaultValue="">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -2,16 +2,40 @@ import {
   Container,
   SearchForm,
   Section,
-  Heading,
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoader, setIsLoader] = useState(false);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const region = searchParams.get('region');
+    if (!region) return;
+    setIsLoader(true);
+    fetchByRegion(region)
+      .then(data => setCountries(data))
+      .finally(() => {
+        setIsLoader(false);
+      });
+  }, [searchParams]);
+
+  const handleSearchSubmit = region => {
+    setSearchParams({ region });
+  };
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSubmit={handleSearchSubmit} />
+        {isLoader && <Loader />}
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
